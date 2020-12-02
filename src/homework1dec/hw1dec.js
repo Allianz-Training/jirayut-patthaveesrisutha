b89e7b4bb9195df0867cce4dce0c595dc1bb00d1
Jirayut <jirayut.patt@gmail.com>
2020-12-02 15:16:59 +0700

    1dec hw and lab2dec

--- src/homework1dec/hw1dec.js
+++ src/homework1dec/hw1dec.js
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-<script>
-    console.log("HW1")
+console.log("HW1")
     array1 = [1,2,30,400]
     array2 = array1.map(item => item*2 )
     console.log(array2)
@@ -83,58 +72,55 @@
     console.log(array2)
 
     array1 = [1,"1",2,{}]
-    array2 = array1.filter(item => item.typeOf(number))
+    array2 = array1.filter(item => typeof item == "number")
     console.log(array2)
     
-    array1 = ["apple","banana","orange"]
-    array2 = array1.map(item => item.toUpperCase())
+    array1 = ["apple","banana","orange","pineapple","watermelon"]
+    array2 = array1.filter(item => item.length > 6)
     console.log(array2)
 
     array1 = [{name: "apple", age: 14},
     {name: "banana", age: 18},
-    {name: "watermelon", age: 32}]
-    array2 = array1.map(item => item.name)
+    {name: "watermelon", age: 32},
+    {name: "pineapple", age: 16},
+    {name: "peach", age: 24}]
+    array2 = array1.filter(item => item.age < 18)
     console.log(array2)
     
     array1 = [{name: "apple", age: 14},
     {name: "banana", age: 18},
-    {name: "watermelon", age: 32}]
-    array2 = array1.map(item => item.age)
-    console.log(array2)
-
-    array1 = [{name: "apple", surname: "London"},
-    {name: "banana",  surname: "Bangkok"},
-    {name: "watermelon",  surname: "Singapore"}]
-    array2 = array1.map(item => item.name + " " + item.surname)
-    console.log(array2)
-
-    array1 = [1,3,4,5,6,7,8]
-    array2 = array1.map(item => (item%2 == 1) ? "odd": "even" )
+    {name: "watermelon", age: 32},
+    {name: "pineapple", age: 16},
+    {name: "peach", age: 24}]
+    array2 = array1.filter(item => item.age == 32)
     console.log(array2)
 
     array1 = [1,-3,2,8,-4,5]
-    array2 = array1.map(item => (item < 0) ? -item: item )
+    array2 = array1.filter(item => item > 0 )
     console.log(array2)
 
-    array1 = [100,200.25,300.84,400.3]
-    array2 = array1.map(item => `${item}` )
+    array1 = [1,3,4,5,6,7,8]
+    array2 = array1.filter(item => item % 3 == 0 )
+    console.log(array2)
+
+    array1 = ["peach", 1, -3, "2", {},[]]
+    array2 = array1.filter(item => typeof item == "string")
+    console.log(array2)
+
+    array1 = ["APPLE", "applE", "PEACH", "PEach"]
+    array2 = array1.filter(item => item == item.toUpperCase())
     console.log(array2)
 
     array1 = [{name: "apple", birth: "2000-01-01"},
     {name: "banana",  birth: "1990-10-01"},
-    {name: "watermelon",  birth: "1985-12-01"}]
-    array2 = array1.map(item => {
-        temp = item.birth.split("-")
-        return {name: item.name,birth: item.birth,age: 2019-temp[0]}
-    })
+    {name: "watermelon",  birth: "1985-12-30"},
+    {name: "peach",  birth: "2002-10-13"}]
+    array2 = array1.filter(item => item.birth.split("-")[1] == 10)
     console.log(array2)
 
     array1 = [{name: "apple", birth: "2000-01-01"},
     {name: "banana",  birth: "1990-10-01"},
-    {name: "watermelon",  birth: "1985-12-01"}]
-    array2 = array1.map(item => {
-        return `"<tr> <td>${item.name}</td> <td>${new Date(item.birth).toUTCString().slice(5,16).toLowerCase()}</td> </tr>"`
-    })
+    {name: "watermelon",  birth: "1985-12-30"},
+    {name: "peach",  birth: "2002-10-13"}]
+    array2 = array1.filter(item => item.birth.split("-")[0] < 2000)
     console.log(array2)
-</script>
-</html>
---
+++ src/lab/lab2dec.js
@@ -0,0 +1,16 @@
+const fruits = ['apple', 'banana', 'orange']
+const students = [{ id: 1, name: 'bean', age: 14 }, { id: 2, name: 'ken', age: 15 }]
+
+const f = [...fruits]
+const s = [...students]
+
+function getFullName({ firstName, lastName }) {
+    return `${firstName} ${lastName}`;
+  }
+
+if(currentUser){
+    test = () => {
+        console.log('Yup.');
+    };
+}
+s.
